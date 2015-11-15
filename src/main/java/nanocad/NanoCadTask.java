dcb4e89d1e1e39a46b8678026ab769f4f766ac2a
scnakandala <supun.nakandala@gmail.com>
2015-11-15 16:49:15 -0500

    adding nanaocad sourcecode

--- src/main/java/nanocad/NanoCadTask.java
+++ src/main/java/nanocad/NanoCadTask.java
@@ -20,9 +20,82 @@
 */
 package nanocad;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import nanocad.GetDataFile;
+import nanocad.nanocadFrame2;
 
-public class NanoCadTask {
-    private final static Logger logger = LoggerFactory.getLogger(NanoCadTask.class);
+import javax.swing.*;
+import java.io.File;
+/**
+ * @author Xiaohai Li
+ *
+ * @see nanocadFrame2
+ * @see MyLongTask
+ * NanocadTask subclasses MyLongTask which uses a SwingWorker to
+ * perform a time-consuming task.
+ *
+ */
+public class NanocadTask extends MyLongTask implements MyLongTaskInterface {
+
+    public static boolean DEBUG = true;
+    private JFrame frame;
+
+    public NanocadTask(JFrame jf) {
+        super();
+        frame = jf;
+    }
+
+    public void go() {
+        current = 0; // from MyLongTask
+        final SwingWorker worker = new SwingWorker() {
+            public Object construct() {
+                return new ActualTask();
+            }
+        };
+        worker.start();
+    }
+
+}
+
+/************************************************************
+ Inner class ActualTask is need for the implementation of
+ a subclass of MyLongTask.  It cannot be made static b/c of
+ the design of MyLongTask
+ */
+final class ActualTask {
+    ActualTask () {
+        //Check if molecular database is available on local machine
+        if (!((new File(Settings.defaultDirStr + Settings.fileSeparator + "common")).exists()))
+        {
+            //nanocadFrame2.progressLabel.setText("Retrieve Molecule Database Progress");
+            String zipFileName =  Settings.getApplicationDataDir() + Settings.fileSeparator + "nanocaddata.zip";
+            if (!((new File(zipFileName)).exists()))
+            {
+                nanocadFrame2.progressLabel.setText("Progress: Retrieve Molecule database from remote system...");
+                GetDataFile gf = new GetDataFile(zipFileName);
+            }
+            //nanocadFrame2.progressLabel.setText("Progress: unzipping molecule database");
+            //ZipExtractor uz = new ZipExtractor(zipFileName);
+            //System.err.println("Done with unzipping nanocaddata.zip");
+        }
+
+        //Check if txt files needed by nanocad is available on local machine
+        if (!((new File(Settings.defaultDirStr + Settings.fileSeparator + "txt")).exists()))
+        {
+            //nanocadFrame2.progressLabel.setText("Retrieve .txt files Progress");
+            String zipFileName = Settings.getApplicationDataDir() + Settings.fileSeparator + "txt.zip";
+            if (!((new File(zipFileName)).exists()))
+            {
+                nanocadFrame2.progressLabel.setText("Progress: Retrieve .txt files from remote system...");
+                GetDataFile gf = new GetDataFile(zipFileName);
+            }
+            //nanocadFrame2.progressLabel.setText("Done Retrieving files");
+            //nanocadFrame2.progressLabel.setText("Progress: unzipping .txt files");
+            //ZipExtractor uz = new ZipExtractor(zipFileName);
+            //System.err.println("Done with unzipping txt.zip");
+        }
+
+        nanocadFrame2.nanocadTask.stop();
+        System.err.println("nanocadTask is done");
+
+    }
 }
