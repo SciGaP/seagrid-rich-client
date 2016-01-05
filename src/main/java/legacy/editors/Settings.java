c3854016f2f293a3425adf44efaaf14416e857b9
scnakandala <supun.nakandala@gmail.com>
2016-01-05 14:16:59 -0500

    testing

--- src/main/java/legacy/editors/Settings.java
+++ src/main/java/legacy/editors/Settings.java
@@ -3,6 +3,7 @@ package legacy.editors;
 import java.io.File;
 
 public class Settings {
+    private static final  boolean LOCALDEBUG = false;
     public static final int ONE_SECOND = 1000;
     public static final String APP_NAME_GAUSSIAN = "GAUSSIAN";
     public static final String APP_NAME_GAMESS = "GAMES";
@@ -13,8 +14,10 @@ public class Settings {
     public static String username = "master";
     public static String defaultDirStr = "";
     public static String fileSeparator = File.separator;
-    private static String applicationDataDir = Settings.class.getResource("/legacy.editors").getPath();
-    public static String jobDir = Settings.class.getResource("/legacy.editors").getPath();
+    private static String applicationDataDir = LOCALDEBUG
+            ? Settings.class.getResource("/legacy.editors").getPath() : "legacy.editors";
+    public static String jobDir = LOCALDEBUG
+            ? Settings.class.getResource("/legacy.editors").getPath() : "legacy.editors";
     public static String httpsGateway = "https://ccg-mw1.ncsa.uiuc.edu/cgi-bin/";
 
     public static Settings getInstance() {
