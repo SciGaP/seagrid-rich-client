bef629b5324d23ca7d136e12474a337bb33baa3b
scnakandala <supun.nakandala@gmail.com>
2016-03-16 15:58:25 -0400

    showing user name in dir browser

--- src/main/java/org/seagrid/desktop/ui/experiment/create/controller/RemoteFilePickerController.java
+++ src/main/java/org/seagrid/desktop/ui/experiment/create/controller/RemoteFilePickerController.java
@@ -23,7 +23,6 @@ package org.seagrid.desktop.ui.experiment.create.controller;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.SftpException;
-import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -206,7 +205,7 @@ public class RemoteFilePickerController {
 
     private void populateRemoteFileTable() throws JSchException, SftpException {
         currentRemoteFileList.clear();
-        fbRemotePath.setText(currentRemotePath.toString());
+        fbRemotePath.setText(SEAGridContext.getInstance().getUserName() + "/" + currentRemotePath.toString());
         FileListModel fileListModel;
         if(currentRemotePath.getParent() != null){
             fileListModel = new FileListModel("..", FileListModel.FileListModelType.PARENT_DIR,0,0, FileListModel.FileLocation.REMOTE,
